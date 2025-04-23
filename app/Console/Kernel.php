36cf21548fcf32a4b7f5bf059903e0e9673d33aa
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-04-23 02:14:31 +0200

    Change CleanupOldInvitations job schedule from daily to every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new CleanupOldInvitations())->everyMinute();
+        $schedule->job(new CleanupOldInvitations())->daily();
     }
 
     /**
