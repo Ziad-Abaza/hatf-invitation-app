7d5f74cc454eb9b88720f2f00f164a54336702de
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-04-28 02:29:28 +0300

    refactor: Comment out userPackage retrieval in returnAction method for improved clarity

--- app/Http/Controllers/Api/V1/UserPaymentController.php
+++ app/Http/Controllers/Api/V1/UserPaymentController.php
@@ -227,7 +227,7 @@ class UserPaymentController extends Controller
 
 
             //    $userPackage= UserPackage::where('payment_user_invitation_id',$payment->id)->first();
-               $userPackage = UserPackage::where('payment_user_invitation_id', $payment->id)->first();
+            //    $userPackage = UserPackage::where('payment_user_invitation_id', $payment->id)->first();
 
             // if($userPackage){
             //     $payment=UserInvitation::where('user_package_id',$userPackage->id)->update([
