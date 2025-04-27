5e66faddb9d69a3917c0baee98e56fe0a0559503
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-04-28 02:30:27 +0300

    refactor: Update userPackage assignment in returnAction method for consistency

--- app/Http/Controllers/Api/V1/UserPaymentController.php
+++ app/Http/Controllers/Api/V1/UserPaymentController.php
@@ -227,7 +227,7 @@ class UserPaymentController extends Controller
 
 
             //    $userPackage= UserPackage::where('payment_user_invitation_id',$payment->id)->first();
-            //    $userPackage = UserPackage::where('payment_user_invitation_id', $payment->id)->first();
+               $userPackage = UserPackage::where('payment_user_invitation_id', $payment->id)->first();
 
             // if($userPackage){
             //     $payment=UserInvitation::where('user_package_id',$userPackage->id)->update([
@@ -245,7 +245,7 @@ class UserPaymentController extends Controller
                     'status' => $status,
                     'user' => $user,
                     'payment' => $payment,
-                    // 'user_package' => $userPackage,
+                    'user_package' => $userPackage,
 
                 ], 200);
             }
