f1cc0103fbd824ba4da326a1065c11a0a760623a
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-04-30 12:46:37 +0300

    refactor: Update invoice PDF file name to a static 'invoice.pdf'

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -339,7 +339,7 @@ if (!function_exists('generateInvoicePDF')) {
 
             // كتابة المحتوى وحفظ PDF
             $mpdf->WriteHTML($html);
-            $filePath = storage_path('app/public/invoices/فاتورة مبيعات.pdf');
+            $filePath = storage_path('app/public/invoices/invoice.pdf');
             $mpdf->Output($filePath, 'F');
 
             return $filePath;
