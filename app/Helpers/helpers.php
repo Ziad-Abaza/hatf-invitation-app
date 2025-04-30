04b4ef1e7d27457db5d717c469179afed0f4ae14
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-05-01 01:57:12 +0300

    refactor: Update parameter for sendInvoiceViaWhatsapp function to use a test value

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -280,7 +280,7 @@ if (!function_exists('sendInvoiceViaWhatsapp')) {
                 'phone' => $phone,
                 'template' => 'qr_invitation_app_invoice_pdf',
                 'pdf' => $invoiceUrl,
-                'param_1' => $param_1,
+                'param_1' => 'test',
             ]);
 
             if ($response->successful()) {
